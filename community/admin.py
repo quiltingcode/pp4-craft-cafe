70b1a2d04e6497e0a19ca03f7a88bb87fe143d87
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-13 15:55:58 +0000

    Install summernote and update admin.py file settings

--- community/admin.py
+++ community/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
