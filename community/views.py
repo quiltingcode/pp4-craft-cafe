6ba2bcc060e6764cc469e1e433d840c48882dbb5
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-16 12:42:01 +0000

    Set up craft community html structure

--- community/views.py
+++ community/views.py
@@ -6,5 +6,5 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'craft-community.html'
     paginate_by = 6
