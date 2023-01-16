04cb08637d2bcf0d67e5d4be869f34ef1429d509
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-16 17:25:40 +0000

    Add paths to project and app urls files to test views render

--- craftcafe/urls.py
+++ craftcafe/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("community.urls"), name="home-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('craft-community/', include('community.urls'), name='community-urls'),
 ]
