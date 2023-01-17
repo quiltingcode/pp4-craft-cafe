7a8273fe17c4453a15929712a50f5ee320417e53
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-17 15:29:22 +0000

    Add post-detail page info to views.py and urls.py to render page correctly

--- community/urls.py
+++ community/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("craft-community/", views.PostList.as_view(), name="community"),
+    path('<slug:slug>/', views.PostDetails.as_view(), name='post-detail'),
 ]
