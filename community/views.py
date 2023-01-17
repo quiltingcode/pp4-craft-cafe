c3bd65206d8e20184156de4a25619dbb5dd683bd
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-17 14:25:22 +0000

    Add views code for post-details page

--- community/views.py
+++ community/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 from django.views.generic import TemplateView
 
@@ -13,3 +13,23 @@ class PostList(generic.ListView):
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class PostDetails(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post-detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
