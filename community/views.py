3366ca00691c8fe45e7513d40a5b923decd5fe0b
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-17 11:15:38 +0000

    Add homepage to urls, and move post views to community page

--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.views.generic import TemplateView
 
 
 class PostList(generic.ListView):
@@ -8,3 +9,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'craft-community.html'
     paginate_by = 6
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
--- craftcafe/urls.py
+++ craftcafe/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from community.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("community.urls"), name="home-urls"),
+    path('craft-community/', include("community.urls"), name="home-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('craft-community/', include('community.urls'), name='community-urls'),
+    # path('craft-community/', include('community.urls'), name='community-urls'),
+    path('', HomeView.as_view(), name='home')
 ]
