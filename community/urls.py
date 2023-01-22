262e8213ba849081c3a4a266c34061027eddf6fe
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-22 11:22:11 +0000

    Bugfix - update name in urls.py to CRAFT-community so that nav link works correctly. #72

--- community/urls.py
+++ community/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
-    path("craft-community/", views.PostList.as_view(), name="community"),
+    path("craft-community/", views.PostList.as_view(), name="craft-community"),
     path('<slug:slug>/', views.PostDetails.as_view(), name='post-detail'),
 ]
