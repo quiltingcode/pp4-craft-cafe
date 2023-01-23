02ec8084fb2a6c8ae06a47f0fa43e7b5e88f2451
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-23 12:38:16 +0000

    Add post_like path into urls.py file

--- community/urls.py
+++ community/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("craft-community/", views.PostList.as_view(), name="craft-community"),
     path('<slug:slug>/', views.PostDetails.as_view(), name='post-detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
