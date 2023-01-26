ae007e85ee9c40d9e5b2508c7198dd7ba201040a
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 16:42:39 +0000

    Add booking function to select available days in views

--- booking/views.py
+++ booking/views.py
@@ -6,3 +6,25 @@ from django.contrib import messages
 
 def contact(request):
     return render(request, "contact.html", {})
+
+
+class BookingView(View):
+    def booking(self, request, *args, **kwargs):
+        weekdays = validWeekday(32)
+        validateWeekdays = isWeekdayValid(weekdays)
+        if request.method == 'POST':
+            workshop = request.POST.get('workshop')
+            day = request.POST.get('day')
+            if workshop is None:
+                messages.success(request, "Please Select A Workshop!")
+                return redirect('booking')
+
+            request.session['day'] = day
+            request.session['workshop'] = workshop
+
+            return redirect('bookingSubmit')
+        
+        return render(request, 'contact.html', {
+            'weekdays': weekdays,
+            'validateWeekdays': validateWeekdays,
+        })
