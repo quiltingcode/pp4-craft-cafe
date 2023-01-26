91c004fd10d5635e438c7792c0b3150160e3a14e
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 17:13:03 +0000

    Add booking forms.py file and connect to views

--- booking/views.py
+++ booking/views.py
@@ -23,8 +23,55 @@ class BookingView(View):
             request.session['workshop'] = workshop
 
             return redirect('bookingSubmit')
-        
+       
         return render(request, 'contact.html', {
             'weekdays': weekdays,
             'validateWeekdays': validateWeekdays,
+        })
+    
+
+    def bookingSubmit(request):
+        user = request.user
+        times = [
+            "4 PM", "6 PM", "10 AM", "11:30 AM"
+        ]
+        today = datetime.now()
+        minDate = today.strftime('%Y-%m-%d')
+        deltatime = today + timedelta(days=21)
+        strdeltatime = deltatime.strftime('%Y-%m-%d')
+        maxDate = strdeltatime
+
+        #Get stored data from django session:
+        day = request.session.get('day')
+        workshop = request.session.get('workshop')
+        
+        #Only show the time of the day that has not been selected before:
+        hour = checkTime(times, day)
+        if request.method == 'POST':
+            time = request.POST.get("time")
+            date = dayToWeekday(day)
+
+            if service != None:
+                if day <= maxDate and day >= minDate:
+                    if date == 'Monday' or date == 'Saturday' or date == 'Wednesday':
+                        if WorkshopBooking.objects.filter(day=day).count() < 11:
+                            if WorkshopBooking.objects.filter(day=day, time=time).count() < 1:
+                                booking_form = BookingForm(data=request.POST)
+                               
+                                messages.success(request, "Appointment Saved!")
+                                return redirect('index')
+                            else:
+                                messages.success(request, "The Selected Time Has Been Reserved Before!")
+                        else:
+                            messages.success(request, "The Selected Day Is Full!")
+                    else:
+                        messages.success(request, "The Selected Date Is Incorrect")
+                else:
+                        messages.success(request, "The Selected Date Isn't In The Correct Time Period!")
+            else:
+                messages.success(request, "Please Select A Service!")
+
+
+        return render(request, 'bookingSubmit.html', {
+            'times':hour,
         })
