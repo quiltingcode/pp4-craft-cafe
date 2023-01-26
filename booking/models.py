3c2ec6261dafdc37581daf3e1c812ff1c75c2a97
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 11:54:58 +0000

    Add workshop categories and time slot options in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+WORKSHOP_CHOICES = (
+    ("All things Wool", "All things Wool"),
+    ("Quilting", "Quilting"),
+    ("Clothing", "Clothing"),
+    ("Home Decor", "Home Decor"),
+    ("Textiles", "Textiles"),
+    ("Kids Crafts", "Kids Crafts"),
+)
+
+TIME_CHOICES = (
+    ("4 PM", "4 PM"),
+    ("6 PM", "6 PM"),
+    ("10AM", "10AM"),
+    ("11:30 AM", "11:30 AM"),
+)
+
+
+class Reservation(models.Model):
+    
