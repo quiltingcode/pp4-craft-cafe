12ffe7749042c738f36aac0485946cbc2ce274a2
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 15:23:56 +0000

    Add total number of places booked to booking model

--- booking/models.py
+++ booking/models.py
@@ -18,6 +18,19 @@ TIME_CHOICES = (
     ("11:30 AM", "11:30 AM"),
 )
 
+PLACES_TO_BOOK = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+    ("6", "6"),
+    ("7", "7"),
+    ("8", "8"),
+    ("9", "9"),
+    ("10", "10"),
+)
+
 
 class WorkshopBooking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -26,6 +39,7 @@ class WorkshopBooking(models.Model):
     time = models.CharField(max_length=10, choices=TIME_CHOICES, default="4 PM")
     created_on = models.DateTimeField(default=datetime.now, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    places = models.CharField(max_length=50, choices=PLACES_TO_BOOK, default="1")
 
     def __str__(self):
         return f"{self.user.username} | day: {self.day} | time: {self.time}"
