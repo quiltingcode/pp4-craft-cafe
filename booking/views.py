5227df751bcce6898e2a3811429f0e311880679e
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 15:18:16 +0000

    Set up view of contact page in views.py

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+from .models import WorkshopBooking
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    return render(request, "contact.html", {})
