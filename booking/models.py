4174331cf66668c841f86e2080f121044454fc3f
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-26 12:41:10 +0000

    Add booking model to booking app

--- booking/models.py
+++ booking/models.py
@@ -19,5 +19,13 @@ TIME_CHOICES = (
 )
 
 
-class Reservation(models.Model):
-    
+class WorkshopBooking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    workshop = models.CharField(max_length=50, choices=WORKSHOP_CHOICES, default="All Things Wool")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="4 PM")
+    created_on = models.DateTimeField(default=datetime.now, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
