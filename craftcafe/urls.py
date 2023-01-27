983a3463a09d6a1424eb832cc058f035a88897f1
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-27 10:55:26 +0000

    Add favicon image and update urls.py

--- craftcafe/urls.py
+++ craftcafe/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from community.views import HomeView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
     path('contact/', include('booking.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
