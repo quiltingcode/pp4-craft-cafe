1db9af01d29f1388e68220b94d5254a5e25e64f2
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-01-31 14:32:12 +0000

    Change contact page view to a classbased view

--- booking/urls.py
+++ booking/urls.py
@@ -2,12 +2,13 @@ from . import views
 from django.urls import path
 from django.views.generic import TemplateView
 from booking.views import WorkshopsView
+from booking.views import ContactPage
 
 urlpatterns = [
 
-    path('contact', views.booking, name='contact'),
+    path('contact', ContactPage.as_view(), name='contact'),
     path('workshops/', WorkshopsView.as_view(), name='workshops'),
-    path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
+    # path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
     # path('user-panel', views.userPanel, name='userPanel'),
     # path('user-update/<int:id>', views.userUpdate, name='userUpdate'),
     # path('user-update-submit/<int:id>', views.userUpdateSubmit, name='userUpdateSubmit'),
--- booking/views.py
+++ booking/views.py
@@ -14,120 +14,120 @@ class WorkshopsView(TemplateView):
     template_name = 'workshops.html'
 
 
-def contact(request):
-    return render(request, "contact.html", {})
+class ContactPage(View):
+
+    def get(self, request):
+        # weekdays = validWeekday(32)
+        # validateWeekdays = isWeekdayValid(weekdays)
+        if request.method == 'POST':
+            workshop = request.POST.get('workshop')
+            day = request.POST.get('day')
+            if workshop is None:
+                messages.success(request, "Please Select A Workshop!")
+                return redirect('booking')
+
+            request.session['day'] = day
+            request.session['workshop'] = workshop
+
+            return redirect('bookingSubmit')
+
+        return render(
+            request,
+            'contact.html',
+            {
+                'booking_form': BookingForm()
+            },
+    )
 
 
-def booking(request):
-    weekdays = validWeekday(32)
-    validateWeekdays = isWeekdayValid(weekdays)
-    if request.method == 'POST':
-        workshop = request.POST.get('workshop')
-        day = request.POST.get('day')
-        if workshop is None:
-            messages.success(request, "Please Select A Workshop!")
-            return redirect('booking')
+# def bookingSubmit(request):
+#     user = request.user
+#     times = [
+#         "4 PM", "6 PM", "10 AM", "11:30 AM"
+#     ]
+#     today = datetime.now()
+#     minDate = today.strftime('%Y-%m-%d')
+#     deltatime = today + timedelta(days=21)
+#     strdeltatime = deltatime.strftime('%Y-%m-%d')
+#     maxDate = strdeltatime
 
-        request.session['day'] = day
-        request.session['workshop'] = workshop
+#     day = request.session.get('day')
+#     workshop = request.session.get('workshop')
 
-        return redirect('bookingSubmit')
+#     hour = checkTime(times, day)
+#     if request.method == 'POST':
+#         time = request.POST.get("time")
+#         date = dayToWeekday(day)
 
-    return render(request, 'contact.html', {
-        'weekdays': weekdays,
-        'validateWeekdays': validateWeekdays,
-        'booking_form': BookingForm()
-    })
-
-
-def bookingSubmit(request):
-    user = request.user
-    times = [
-        "4 PM", "6 PM", "10 AM", "11:30 AM"
-    ]
-    today = datetime.now()
-    minDate = today.strftime('%Y-%m-%d')
-    deltatime = today + timedelta(days=21)
-    strdeltatime = deltatime.strftime('%Y-%m-%d')
-    maxDate = strdeltatime
-
-    day = request.session.get('day')
-    workshop = request.session.get('workshop')
-
-    hour = checkTime(times, day)
-    if request.method == 'POST':
-        time = request.POST.get("time")
-        date = dayToWeekday(day)
-
-        if service is not None:
-            if day <= maxDate and day >= minDate:
-                if date == 'Monday' or date == 'Saturday' or date == 'Wednesday':
-                    if WorkshopBooking.objects.filter(day=day).count() < 11:
-                        if WorkshopBooking.objects.filter(day=day, time=time).count() < 1:
+#         if workshop is not None:
+#             if day <= maxDate and day >= minDate:
+#                 if date == 'Monday' or date == 'Saturday' or date == 'Wednesday':
+#                     if WorkshopBooking.objects.filter(day=day).count() < 11:
+#                         if WorkshopBooking.objects.filter(day=day, time=time).count() < 1:
                         
-                            booking_form = BookingForm(data=request.POST)
+#                             booking_form = BookingForm(data=request.POST)
                             
-                            messages.success(request, "Booking Saved!")
-                            return redirect('contact')
-                        else:
-                            messages.success(request, "The Selected Time slot is full!")
-                    else:
-                        messages.success(request, "The Selected Day Is Full!")
-                else:
-                    messages.success(request, "The Selected Date Is Incorrect")
-            else:
-                messages.success(request, "The Selected Date Isn't In The Correct Time Period!")
-        else:
-            messages.success(request, "Please Select A Workshop!")
+#                             messages.success(request, "Booking Saved!")
+#                             return redirect('contact')
+#                         else:
+#                             messages.success(request, "The Selected Time slot is full!")
+#                     else:
+#                         messages.success(request, "The Selected Day Is Full!")
+#                 else:
+#                     messages.success(request, "The Selected Date Is Incorrect")
+#             else:
+#                 messages.success(request, "The Selected Date Isn't In The Correct Time Period!")
+#         else:
+#             messages.success(request, "Please Select A Workshop!")
 
-    return render(request, 'bookingSubmit.html', {
-        'times': hour,
-    })
-
-
-def isWeekdayValid(x):
-    validateWeekdays = []
-    for j in x:
-        if WorkshopBooking.objects.filter(day=j).count() < 10:
-            validateWeekdays.append(j)
-    return validateWeekdays
+#     return render(request, 'bookingSubmit.html', {
+#         'times': hour,
+#     })
 
 
-def dayToWeekday(x):
-    z = datetime.strptime(x, "%Y-%m-%d")
-    y = z.strftime('%A')
-    return y
+# def isWeekdayValid(x):
+#     validateWeekdays = []
+#     for j in x:
+#         if WorkshopBooking.objects.filter(day=j).count() < 10:
+#             validateWeekdays.append(j)
+#     return validateWeekdays
 
 
-def validWeekday(days):
-
-    today = datetime.now()
-    weekdays = []
-    for i in range(0, days):
-        x = today + timedelta(days=i)
-        y = x.strftime('%A')
-        if y == 'Monday' or y == 'Saturday' or y == 'Wednesday':
-            weekdays.append(x.strftime('%Y-%m-%d'))
-    return weekdays
+# def dayToWeekday(x):
+#     z = datetime.strptime(x, "%Y-%m-%d")
+#     y = z.strftime('%A')
+#     return y
 
 
-# Only show the time of the day that has not been selected before:
-def checkTime(times, day):
+# def validWeekday(days):
 
-    x = []
-    for k in times:
-        if WorkshopBooking.objects.filter(day=day, time=k).count() < 1:
-            x.append(k)
-    return x
+#     today = datetime.now()
+#     weekdays = []
+#     for i in range(0, days):
+#         x = today + timedelta(days=i)
+#         y = x.strftime('%A')
+#         if y == 'Monday' or y == 'Saturday' or y == 'Wednesday':
+#             weekdays.append(x.strftime('%Y-%m-%d'))
+#     return weekdays
 
 
-# Only show the time of the day that has not been selected before:
-def checkEditTime(times, day, id):
+# # Only show the time of the day that has not been selected before:
+# def checkTime(times, day):
 
-    x = []
-    booking = WorkshopBooking.objects.get(pk=id)
-    time = booking.time
-    for k in times:
-        if WorkshopBooking.objects.filter(day=day, time=k).count() < 1 or time == k:
-            x.append(k)
-    return x
+#     x = []
+#     for k in times:
+#         if WorkshopBooking.objects.filter(day=day, time=k).count() < 1:
+#             x.append(k)
+#     return x
+
+
+# # Only show the time of the day that has not been selected before:
+# def checkEditTime(times, day, id):
+
+#     x = []
+#     booking = WorkshopBooking.objects.get(pk=id)
+#     time = booking.time
+#     for k in times:
+#         if WorkshopBooking.objects.filter(day=day, time=k).count() < 1 or time == k:
+#             x.append(k)
+#     return x
