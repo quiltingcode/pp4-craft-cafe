38c0d8c55d6e7b8b93a184a7614f73ed67b0732a
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-02-09 08:57:32 +0000

    Set up url paths for edit and delete posts

--- community/urls.py
+++ community/urls.py
@@ -1,10 +1,13 @@
 from . import views
 from django.urls import path
+from community.views import EditPost, DeletePost
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("craft-community/", views.PostList.as_view(), name="craft-community"),
     path('<slug:slug>/', views.PostDetails.as_view(), name='post-detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('like/<slug:slug>', views.PostLike2.as_view(), name='post_like2'),
+    path('like2/<slug:slug>', views.PostLike2.as_view(), name='post_like2'),
+    path('edit-post/<int:pk>', EditPost.as_view(), name='edit-post'),
+    path('delete-post/<int:pk>', DeletePost.as_view(), name='delete-post'),
 ]
