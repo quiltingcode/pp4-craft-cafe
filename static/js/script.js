0d0575f2383998979ce3ebef4bf9e4b16c2fcd0f
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-02-15 12:26:30 +0000

    Set up dropdown reset function if workshop is re-selected

--- static/js/script.js
+++ static/js/script.js
@@ -10,11 +10,22 @@ $(document).ready(function() {
         alert.close();
     }, 3000);
 
+    /* Initial reset state of all fields before selection */
+
+    var dayDropdownValues = $('#id_day').html();
+    var timeDropdownValues = $('#id_time').html();
+    var placesDropdownValues = $('#id_places').html();
+
 
     /* Booking Form Validation - check which workshop option has been selected */
 
 
     $('#id_workshop').change(function(){ 
+        /* Reset fieds each time workshop field is changed */
+        $('#id_day').html(dayDropdownValues);
+        $('#id_time').html(timeDropdownValues);
+        $('#id_places').html(placesDropdownValues);
+        /* Subsequent dropdown changes based on workshop dropdown selection */
         if ($(this).val() == 'All things Wool'){
             console.log(this.value)
             enableMondays()
@@ -22,18 +33,23 @@ $(document).ready(function() {
         } else if ($(this).val() == 'Quilting'){
             console.log(this.value)
             enableTuesdays()
+            removeMorning()
         } else if ($(this).val() == 'Clothing'){
             console.log(this.value)
             enableWednesdays()
+            removeMorning()
         } else if ($(this).val() == 'Home Crafts'){
             console.log(this.value)
             enableThursdays()
+            removeMorning()
         } else if ($(this).val() == 'Needlepoint'){
             console.log(this.value)
             enableFridays()
+            removeMorning()
         } else if ($(this).val() == 'Kids Crafts'){
             console.log(this.value)
             enableSaturdays()
+            removeAfternoon()
         } else {
             disableAllDays()
         }
@@ -144,16 +160,51 @@ $(document).ready(function() {
         }
     }
 
+    function disableAllDays() {
+        $("#id_day").datepicker({
+            dateFormat: 'dd-mm-yy',
+            minDate: +0,
+            maxDate: '+5M',
+            beforeShowDay: disableall
+        });
+    };
+
+    function disableall(in_date) {
+        if (in_date.getDay() == 1) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 2) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 3) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 4) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 5) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 6) {
+            return [false, "notav", 'Not Available'];
+        } else if (in_date.getDay() == 0) {
+            return [false, "notav", 'Not Available'];
+        } else {
+            return [true, "av", "available"];
+        }
+    }
+
     /* If Monday - Friday workshop selected, only show afternoon time slots */
 
     function removeMorning() {
         $("#id_time option[value='10 - 11:30am']").remove();
         $("#id_time option[value='11:30am - 1pm']").remove();
+        $("#id_time option[value='4 - 6pm']").add();
+        $("#id_time option[value='6 - 8pm']").add();
     }
+
+    /* If Saturday workshop selected, only show morning time slots */
 
     function removeAfternoon() {
         $("#id_time option[value='4 - 6pm']").remove();
-        $("#id_time option[value='6 - 8 pm']").remove();
+        $("#id_time option[value='6 - 8pm']").remove();
+        $("#id_time option[value='10 - 11:30am']").add();
+        $("#id_time option[value='11:30am - 1pm']").add();
     }
 
   
