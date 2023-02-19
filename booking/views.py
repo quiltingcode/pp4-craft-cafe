a8d8a706ab0ff322b51bd664947f5392d6881f4e
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-02-19 16:29:08 +0000

    Add pricing to workshop info

--- booking/views.py
+++ booking/views.py
@@ -76,32 +76,6 @@ class EditBooking(UpdateView):
     fields = ['workshop', 'day', 'time', 'places',]
     success_url = '/contact/profile-page'
 
-    # def post(self, request):
-    #     time = request.POST.get("time")
-    #     day = request.session.get('day')
-    #     places = request.POST.get("places")
-    #     booking_form = BookingForm(data=request.POST)
-    #     num_bookings = WorkshopBooking.objects.filter(day=day, time=time).count()
-    #     if num_bookings + int(places) <= 10:    
-    #         if booking_form.is_valid():
-    #             booking = booking_form.save(commit=False)
-    #             booking.user = User.objects.get(id=request.user.id)
-    #             booking.email = request.user.email
-    #             booking.name = request.user.username
-    #             booking.approved = False
-    #             booking.save()
-    #             messages.add_message(
-    #                 request, messages.SUCCESS,
-    #                 'Booking updated successfully, awaiting re-approval.')
-    #         else:
-    #             booking_form = BookingForm()
-    #     else:
-    #         messages.add_message(
-    #             request, messages.SUCCESS,
-    #             'The selected date is full. please try a different session.')  
-
-    #     return redirect('profile-page')
-
 
 class DeleteBooking(DeleteView):
     model = WorkshopBooking
@@ -151,6 +125,31 @@ class AdminEditBooking(UpdateView):
     template_name = 'admin-edit-booking.html'
     fields = ['workshop', 'day', 'time', 'places', 'approved']
     success_url = reverse_lazy('cafe-dashboard')
+
+    def post(self, request, pk):
+        time = request.POST.get("time")
+        day = request.session.get('day')
+        places = request.POST.get("places")
+        booking_form = BookingForm(data=request.POST)
+        num_bookings = WorkshopBooking.objects.filter(day=day, time=time).count()
+        if num_bookings + int(places) <= 10:   
+            if booking_form.is_valid():
+                booking = booking_form.save(commit=False)
+                booking.user = User.objects.get(id=request.user.id)
+                booking.email = request.user.email
+                booking.name = request.user.username
+                booking.save()
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    'Booking updated successfully, awaiting re-approval.')
+            else:
+                booking_form = BookingForm()
+        else:
+            messages.add_message(
+                request, messages.SUCCESS,
+                'The selected date is full. please try a different session.')  
+
+        return redirect('profile-page')
 
 
 class AdminDeleteBooking(DeleteView):
