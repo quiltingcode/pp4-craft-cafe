cdc345d9c26aa47b296a1ea10239636e8713cfcc
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-02-27 18:12:55 +0000

    Change debug settings

--- booking/views.py
+++ booking/views.py
@@ -191,10 +191,8 @@ class AdminDeleteBooking(DeleteView):
 
 
 def error_404(request, exception):
-    """ 404 error page """
-    return render(request, '404.html', status=404)
+    return render(request, '404.html')
 
 
 def error_500(request):
-    """ 500 error page """
-    return render(request, '500.html', status=500)
+    return render(request, '500.html')
